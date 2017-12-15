ccc5f744d8b46f3756ad8b4fd87036111a176e84
Cjay <billonesciprian08@gmail.com>
2017-12-15 18:58:39 +0800

    Add view for traffic violations

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,6 +3,10 @@ class StaticPagesController < ApplicationController
   end
 
   def traffic_violations
+    @base_path = 'public/images/catchall'
+    @raw_images = Dir.glob(@base_path + '/raw_image/*')
+    @license_plates = Dir.glob(@base_path + '/license_plate/*')
+    @count = (@raw_images.count == @license_plates.count) ? @raw_images.count : nil
   end
 
   def reports
