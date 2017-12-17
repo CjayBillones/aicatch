114b61324b1d63826db7b68f9a86e2f5f7834687
Cjay <billonesciprian08@gmail.com>
2017-12-17 19:13:09 +0800

    Add single view of cameras

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,14 @@
 class StaticPagesController < ApplicationController
+  
   def live
+  
+  end
+
+  def single_live
+    #render plain: params[:camera].inspect
+    @source = params[:src]
+    @camera = params[:camera]
+    @all_captured_violators = CapturedViolator.all
   end
 
   def traffic_violations
@@ -11,4 +20,5 @@ class StaticPagesController < ApplicationController
   def reports
     @all_captured_violators = CapturedViolator.all
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get '/live' => 'static_pages#live'
   get '/traffic_violations' => 'static_pages#traffic_violations'
   get '/reports' => 'static_pages#reports'
+  get '/live-single-view' => 'static_pages#single_live'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
