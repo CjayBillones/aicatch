cecd9e77c17e1ae56713215007dbc6fa22643385
Cjay <billonesciprian08@gmail.com>
2018-02-04 05:51:48 +0800

    Add tests for location

--- test/models/location_test.rb
+++ test/models/location_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class LocationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @location = Location.new(name: "Camera 1")
+  end
+
+  test "should be valid" do
+    assert @location.valid?
+  end
+
+  test "name should be unique" do
+    duplicate_location = @location.dup
+    duplicate_location.name = @location.name.upcase
+    @location.save
+    assert_not duplicate_location.valid?
+  end
+
 end
