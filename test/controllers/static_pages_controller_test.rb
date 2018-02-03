9635ad0b4832f60596066f72da90de665329290f
Cjay <billonesciprian08@gmail.com>
2018-02-04 03:15:33 +0800

    Fix failing test

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,22 +2,22 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should redirect to login page when accessing live page by public user" do
-    get live_url
+    get live_path
     assert_redirected_to  login_path
   end
 
   test "should redirect to login page when accessing single live page by public user" do
-    get live_single_view_url
+    get single_live_path
     assert_redirected_to login_path
   end
 
   test "should redirect to login page when accessing traffic_violations page by public user" do
-    get traffic_violations_url
+    get traffic_violations_path
     assert_redirected_to login_path
   end
 
   test "should redirect to login page when accessing reports page by public user" do
-    get reports_url
+    get reports_path
     assert_redirected_to login_path
   end
 
